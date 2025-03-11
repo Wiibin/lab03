d3de83e1b525a6488bc81eec1fd179fd56b2b792
Wiibin <m9134538978@gmail.com>
2025-03-11 22:26:41 +0300

    Update print.hpp

--- include/print.hpp
+++ include/print.hpp
@@ -3,4 +3,4 @@
 #include <string>
 
 void print(const std::string& text, std::ofstream& out);
-void print(const std::string& text, std::ofstream& out = std::cout);
+void print(const std::string& text, std::ostream& out = std::cout);
