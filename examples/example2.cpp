cd2100763168d32bad337f84d9ba110c62796c47
Wiibin <m9134538978@gmail.com>
2025-03-11 22:27:22 +0300

    Update example2.cpp

--- examples/example2.cpp
+++ examples/example2.cpp
@@ -1,8 +1,9 @@
 #include <print.hpp>
 
-#incude <fstream>
+#include <fstream>
+
 int main(int argc, char** argv)
 {
-std::ofstream file("log.txt");
-print(std::string("hello"), file);
+  std::ofstream file("log.txt");
+  print(std::string("hello"), file);
 }
